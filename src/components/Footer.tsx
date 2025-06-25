0300efc3c7215b4570d980cbc586ef7f505eedf7
DeLazzari808 <joaopedrodelazzari2@gmail.com>
2025-06-25 12:24:17 -0300

    feat: update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -105,7 +105,6 @@ const Footer = () => {
             </p>
             <p className="flex items-center space-x-1 text-sm text-gray-400 dark:text-gray-300">
               <span>Made with</span>
-              <Heart size={16} className="text-red-500" />
               <span>using React & TailwindCSS</span>
             </p>
           </div>
